890489363c83a1ffcfef30fdc7fcd2b34a99b3ce
cdanmar <cdanmar@gmail.com>
2017-08-05 21:51:39 -0500

    pre bootstrap testing

---
+++ app/controllers/artworks_controller.rb
@@ -0,0 +1,30 @@
+class ArtworksController < ApplicationController
+  def index
+    @artworks = Artwork.all
+  end
+
+  def show
+    @artwork = Artwork.find(params[:id])
+  end
+
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+  end
+
+  def edit
+    @artwork = Artwork.find_by(id: params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
 
+  get '/' => 'artworks#index'
+
+  get '/artworks' => 'artworks#index'
+  get '/artworks/new' => 'artworks#new'
+  post '/artworks' => 'artworks#create'
+  get '/artworks/:id' => 'artworks#show'
+  get '/artworks/:id/edit' => 'artworks#edit'
+  patch '/artworks/:id' => 'artworks#update'
+  delete '/artworks/:id' => 'artworks#destroy'
+
+
   
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
